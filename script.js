6910f5a95a6e632b7b288428d1ff2c8259fc607d
salman-41 <salman.yz@outlook.com>
2023-12-03 20:35:23 +0500

    feat: modalClose events added

--- script.js
+++ script.js
@@ -14,3 +14,6 @@ function showModal() {
 
 //Modal Event Listeners
 modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () =>
+  modal.classList.remove('show-modal')
+);
