899d98a9aabbbd6b47663d9b3619b6089b3cd241
salman-41 <salman.yz@outlook.com>
2023-12-04 20:05:08 +0500

    feat: handle data from From

--- script.js
+++ script.js
@@ -21,5 +21,12 @@ window.addEventListener('click', e =>
   e.target == modal ? modal.classList.remove('show-modal') : false
 );
 
+// Handle Data from Form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+}
+
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
