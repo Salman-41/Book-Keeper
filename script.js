bb4f3ce593a157194042a0fcae0d53f49aac6ac4
salman-41 <salman.yz@outlook.com>
2023-12-06 20:19:59 +0500

    feat: delete functionilty added

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
-const websiteNameEl = document.getElementById('website-name');
 const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
+
+let bookmarks = [];
 
 // Show Modal, Focus on Input
 function showModal() {
@@ -12,14 +14,31 @@ function showModal() {
   websiteNameEl.focus();
 }
 
-//Modal Event Listeners
+// Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () =>
   modal.classList.remove('show-modal')
 );
 window.addEventListener('click', e =>
-  e.target == modal ? modal.classList.remove('show-modal') : false
+  e.target === modal ? modal.classList.remove('show-modal') : false
 );
+
+// Validate Form
+function validate(nameValue, urlValue) {
+  const expression =
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields.');
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address.');
+    return false;
+  }
+  // Valid
+  return true;
+}
 
 // Build Bookmarks
 function buildBookmarks() {
@@ -58,25 +77,8 @@ function buildBookmarks() {
   });
 }
 
-// Validate Form
-function validate(nameValue, urlValue) {
-  const expression =
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
-  const regex = new RegExp(expression);
-  if (!nameValue || !urlValue) {
-    alert('Please submit values for both fields.');
-    return false;
-  }
-  if (!urlValue.match(regex)) {
-    alert('Please provide a valid web address');
-    return false;
-  }
-  // Valid
-  return true;
-}
-
-//Fetch Bookmarks
-const fetchBookmarks = function () {
+// Fetch bookmarks
+function fetchBookmarks() {
   // Get bookmarks from localStorage if available
   if (localStorage.getItem('bookmarks')) {
     bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
@@ -84,30 +86,47 @@ const fetchBookmarks = function () {
     // Create bookmarks array in localStorage
     bookmarks = [
       {
-        name: 'salman portfolio',
-        url: 'https://portfolio.salmanyz',
+        name: 'Jacinto Design',
+        url: 'http://jacinto.design',
       },
     ];
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
-};
+  buildBookmarks();
+}
 
-// Handle Data from Form
+// Delete Bookmark
+function deleteBookmark(url) {
+  // Loop through the bookmarks array
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
 function storeBookmark(e) {
   e.preventDefault();
   const nameValue = websiteNameEl.value;
   let urlValue = websiteUrlEl.value;
-  if (!urlValue.includes('http://', 'https://')) {
+  // Add 'https://' if not there
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
     urlValue = `https://${urlValue}`;
   }
+  // Validate
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  // Set bookmark object, add to array
   const bookmark = {
     name: nameValue,
     url: urlValue,
   };
   bookmarks.push(bookmark);
+  // Set bookmarks in localStorage, fetch, reset input fields
   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   fetchBookmarks();
   bookmarkForm.reset();
